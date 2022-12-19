b43a29786f351b6ecab7b4ccd473ae09e5d0ab23
George FunBook <gkurelic@gmail.com>
2022-12-18 23:21:01 -0600

    add template state

---
+++ source/states/rooms/TemplateState.hx
@@ -0,0 +1,21 @@
+package states.rooms;
+
+import flixel.FlxG;
+
+class TemplateState extends SmoothRoomState
+{
+    override function create()
+    {
+        super.create();
+    }
+    
+    override function initEntities()
+    {
+        super.initEntities();
+    }
+    
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+    }
+}
